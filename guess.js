9e8b7ea2e1187280a885af4a7bf3e2f074008eaa
sylva <atlaouimassilva@gmail.com>
2026-06-16 13:00:35 +0100

    Implement input validation in guess function
    Added input validation for user guesses in the guessing game.

--- guess.js
+++ guess.js
@@ -1,28 +1,33 @@
+
 const min = 1;
 const max  = 100;
-let number = Math.random() *(max -min +1);
+let number = Math.random() *(max -min +1) + min;
 
 function ggg(){
     let myguess = Number(document.getElementById("num").value);
+    if(isNaN(myguess)){
+        document.getElementById("answer").textContent = "Please enter a valid number";
+    }else if(myguess > max || myguess <min){
+                document.getElementById("answer").textContent = "Please enter a valid number";
 
-    if(myguess == number){
 
-        document.getElementById("answer").textContent = " GG ! click on replay to play another round";
+    }else {
+            if(myguess == number){
 
-    }else if(number<myguess){
-        document.getElementById("answer").textContent = " TOO HIGH ! TRY AGIAN";
-    }else if(number>myguess){
-        document.getElementById("answer").textContent = " TOO LOW ! TRY AGIAN";
+            document.getElementById("answer").textContent = " GG ! click on replay to play another round";
+
+        }else if(number<myguess){
+            document.getElementById("answer").textContent = " TOO HIGH ! TRY AGIAN";
+        }else if(number>myguess){
+            document.getElementById("answer").textContent = " TOO LOW ! TRY AGIAN";
+        }
     }
 }
+
 function round(){
     number = Math.floor(Math.random() * 100) + 1;
     document.getElementById("num").value = "";
     document.getElementById("answer").textContent = "";
     
 }
-document.getElementById("num").addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-        ggg();
-    }
-});
+
